c2fb33d1dfebb495520f788c71cf0f0ee244e828
Anvay Shah <anvay@Cerebro.local>
2023-06-02 13:41:34 +0530

    Changed run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,12 +2,7 @@
 
 for INDEX in '1' '2' '3' '4' '5' '6'
 do
-    file="results/Baseline3/octmnist/oct_dispsum_baseline3_run$INDEX-epochs=10_8.txt"
-    python3 src/fed_main.py --dataset=octmnist --epochs=10 --local_ep=8 --model=cnn --subset=True --random=False --algo=dispsum > $file
+    file="results/Baseline2/oct_baseline2_run$INDEX-epochs=8.txt"
+    python3 src/baseline_main.py --dataset=octmnist --epochs=8 --model=cnn > $file
 done
 
-for INDEX in '1' '2' '3' '4' '5' '6'
-do
-    file="results/Baseline3/octmnist/oct_dispmin_baseline3_run$INDEX-epochs=10_8.txt"
-    python3 src/fed_main.py --dataset=octmnist --epochs=10 --local_ep=8 --model=cnn --subset=True --random=False --algo=dispmin > $file
-done
